742ce7e89ec6ab35e4f774144d00462580ffe24d
Ryan Wilson <47390291+dev-PB@users.noreply.github.com>
2024-11-30 13:45:48 +0000

    Add linting and automatic LSP setups

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,35 @@
+return {
+    "mfussenegger/nvim-lint",
+
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+
+    config = function()
+        local linter = require("lint")
+
+        linter.linters_by_ft = {
+            python = { "pylint" },
+        }
+
+        local lint_augroup = vim.api.nvim_create_augroup(
+            "lint", { clear = true }
+        )
+
+        vim.api.nvim_create_autocmd(
+            {
+                "BufEnter",
+                "BufWritePost",
+                "InsertLeave"
+            },
+            {
+                group = lint_augroup,
+                callback = function()
+                    linter.try_lint()
+                end
+            }
+        )
+    end
+
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -8,21 +8,30 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
 
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                }
+			        "pylint",
+                },
+            })
+
+            require("mason-lspconfig").setup_handlers({
+                -- Default LSP handler
+                function(ls_name)
+                    require("lspconfig")[ls_name].setup({})
+                end,
+
+                -- Language specific handlers
+                ["rust-analyzer"] = function()
+                    require("rust-tools").setup({})
+                end,
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
-
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-        end
     }
 }
