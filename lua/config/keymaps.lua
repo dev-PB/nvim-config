c3674882aed4b43b66a3a77ec9b782733d535912
Ryan Wilson <47390291+dev-PB@users.noreply.github.com>
2024-11-30 04:35:59 +0000

    Add LSPs and configure LSP keymaps.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
-
+-- Telescope
 local telescope = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', telescope.find_files, {})
+
+-- LSP
+vim.keymap.set('n', 'O', vim.lsp.buf.hover, {})
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
