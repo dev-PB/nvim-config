022214ad7b1ac14c0c7797387e8f65df86a31eff
Ryan Wilson <47390291+dev-PB@users.noreply.github.com>
2024-12-01 02:54:02 +0000

    Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,15 +1,15 @@
 -- File Search
 local telescope = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', telescope.find_files, {})
+vim.keymap.set('n', '<C-t>', telescope.find_files, {})
 
 -- File Explorer
-vim.keymap.set('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-p>', '<Cmd>NvimTreeToggle<CR>')
 
 -- LSP
-vim.keymap.set('n', 'O', vim.lsp.buf.hover, {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set({'n', 'v'}, '<leader><CR>', vim.lsp.buf.code_action, {})
+vim.keymap.set('n', '<M-i>', vim.lsp.buf.hover, {})
+vim.keymap.set('n', '<F12>', vim.lsp.buf.definition, {})
+vim.keymap.set({'n', 'v'}, '<M-.>', vim.lsp.buf.code_action, {})
 
 -- ToggleTerm
 local custom_functions = require("config.custom_functions");
-vim.keymap.set('n', '<C-t>', custom_functions.toggle_or_create_terminal);
+vim.keymap.set('n', '<C-n>', custom_functions.toggle_or_create_terminal);
