674c39df83b113f60de9f1ce2652cd349e17b4fc
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-03-26 09:56:11 +0000

    Add keybind to easily minimize the floating terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,7 @@ local telescope = require("telescope.builtin")
 vim.keymap.set('n', '<C-t>', telescope.find_files, {})
 
 -- File Explorer
-vim.keymap.set('n', '<C-p>', '<Cmd>NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-b>', '<Cmd>NvimTreeToggle<CR>')
 
 -- LSP
 vim.keymap.set('n', '<M-i>', vim.lsp.buf.hover, {})
@@ -13,3 +13,4 @@ vim.keymap.set({'n', 'v'}, '<M-.>', vim.lsp.buf.code_action, {})
 -- ToggleTerm
 local custom_functions = require("config.custom_functions");
 vim.keymap.set('n', '<C-n>', custom_functions.toggle_or_create_terminal);
+vim.api.nvim_set_keymap('t', '<C-n>', '<C-\\><C-n>:ToggleTerm<CR>', {noremap = true, silent = true})
