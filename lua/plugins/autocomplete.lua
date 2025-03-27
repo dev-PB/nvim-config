7a25e390b76df04cc771fd90762178f17688c1ae
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-03-27 17:02:04 +0000

    Scroll through autocomplete suggestions with Tab

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -18,6 +18,7 @@ return {
 		config = function()
 			-- Set up nvim-cmp.
 			local cmp = require'cmp'
+			local luasnip = require'luasnip'
 
 			require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -35,12 +36,36 @@ return {
 				},
 
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-Space>'] = cmp.mapping.complete(),
 					['<C-e>'] = cmp.mapping.abort(),
 					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				}),
+
+					["<Tab>"] = cmp.mapping(
+						function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.expand_or_jumpable() then
+								luasnip.expand_or_jump()
+							else
+								fallback()
+							end
+						end,
+						{"i", "s"}
+					),
+
+					["<S-Tab>"] = cmp.mapping(
+						function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.jumpable(-1) then
+								luasnip.jump(-1)
+							else
+								fallback()
+							end
+						end,
+						{"i", "s"}
+					),
+                }),
 
 				sources = cmp.config.sources(
 					{
