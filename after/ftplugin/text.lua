247d248f86d32f96f878ad99c0e4c1ee6e36f8d0
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-04-02 12:11:40 +0100

    Make .txt & .md specific options local

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,7 +1,7 @@
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.colorcolumn = ""
-vim.opt.breakindent = true
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
+vim.opt_local.colorcolumn = ""
+vim.opt_local.breakindent = true
 
-vim.keymap.set({'n', 'v'}, 'j', 'gj', { remap = true })
-vim.keymap.set({'n', 'v'}, 'k', 'gk', { remap = true })
+vim.keymap.set({'n', 'v'}, 'j', 'gj', { remap = true, silent = true })
+vim.keymap.set({'n', 'v'}, 'k', 'gk', { remap = true, silent = true })
