0eb4db51dcd505fb2d1224cff7dfa7b4797ebc25
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-04-02 23:41:01 +0100

    Add a bunch of new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,14 +12,40 @@ vim.keymap.set('n', '<F12>', vim.lsp.buf.definition, { desc = "Go to Definition"
 vim.keymap.set({'n', 'v'}, '<M-.>', vim.lsp.buf.code_action, { desc = "Code Actions"} )
 vim.keymap.set({'n', 'i'}, '<S-F12>', telescope.lsp_references, { desc = "Find References"} )
 
--- ToggleTerm
+-- Open ToggleTerm
 local custom_functions = require("config.custom_functions");
-vim.keymap.set('n', '<C-n>', custom_functions.toggle_or_create_terminal, { desc = "Open the terminal"} );
-vim.api.nvim_set_keymap('t', '<C-n>', '<C-\\><C-n>:ToggleTerm<CR>', {noremap = true, silent = true})
+vim.keymap.set('n', '<C-n>', custom_functions.toggle_or_create_terminal, { desc = "Open the terminal" });
 
--- Comments
-vim.keymap.set('v', '<C-_>', "gc", { remap = true, desc = "Toggle comment" })
-vim.keymap.set('n', '<C-_>', "gcc", { remap = true, desc = "Toggle comment" })
+-- Exit ToggleTerm
+vim.api.nvim_set_keymap('t', '<C-n>', '<C-\\><C-n>:ToggleTerm<CR>', { noremap = true, silent = true })
+
+-- Comments, <C-_> = <C-/>
+vim.keymap.set('v', '<C-_>', "gc", { remap = true, silent = true, desc = "Toggle comment" })
+vim.keymap.set('n', '<C-_>', "gcc", { remap = true, silent = true, desc = "Toggle comment" })
 
 -- Delete last word in insert mode, <C-H> = <C-BS>
-vim.keymap.set('i', '<C-H>', "<C-W>", {})
+vim.keymap.set('i', '<C-H>', "<C-W>", { noremap = true, silent = true })
+
+-- Move text up/down
+vim.keymap.set('n', '<M-k>', ':m -2<CR>', { noremap = true, silent = true, desc = "Move line up" })
+vim.keymap.set('n', '<M-j>', ':m +1<CR>', { noremap = true, silent = true, desc = "Move line down" })
+vim.keymap.set('v', '<M-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move selection up" })
+vim.keymap.set('v', '<M-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move selection down" })
+vim.keymap.set('i', '<M-j>', '<Cmd>:m +1<CR>', { noremap = true, silent = true, desc = "Move line down" })
+vim.keymap.set('i', '<M-k>', '<Cmd>:m -2<CR>', { noremap = true, silent = true, desc = "Move line up" })
+
+-- Indent selection & keep selected
+vim.keymap.set('v', '<Tab>', '>gv', { noremap = true, silent = true, desc = "Indent selection" })
+vim.keymap.set('v', '<S-Tab>', '<gv', { noremap = true, silent = true, desc = "Unindent selection" })
+
+-- Window navigation
+vim.keymap.set('n', '<Leader>h', '<C-w>h', { noremap = true, silent = true, desc = "Focus left window" })
+vim.keymap.set('n', '<Leader>j', '<C-w>j', { noremap = true, silent = true, desc = "Focus below window" })
+vim.keymap.set('n', '<Leader>k', '<C-w>k', { noremap = true, silent = true, desc = "Focus above window" })
+vim.keymap.set('n', '<Leader>l', '<C-w>l', { noremap = true, silent = true, desc = "Focus right window" })
+
+-- Resize windows
+vim.keymap.set('n', '<M-Up>', ':resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Down>', ':resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<M-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
