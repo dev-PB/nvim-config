a08fa94047724732ac63c37f7c9018335620a481
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-04-02 11:54:52 +0100

    When wrapping is enabled, make j/k go to next/prev displayed line

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,6 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.colorcolumn = ""
 vim.opt.breakindent = true
+
+vim.keymap.set({'n', 'v'}, 'j', 'gj', { remap = true })
+vim.keymap.set({'n', 'v'}, 'k', 'gk', { remap = true })
