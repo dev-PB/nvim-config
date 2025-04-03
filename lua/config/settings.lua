9e0c9bdfef4562a37dcbc91f3f6118c09f852b97
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-04-03 02:49:23 +0100

    Rearrange settings, replace vim.cmd with vim.opt, set showmode = false

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -3,27 +3,27 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- General
-vim.opt.title = true
-vim.opt.termguicolors = true
 vim.opt.swapfile = false
 vim.opt.undofile = true
-vim.opt.guicursor = ""
-vim.opt.colorcolumn = "120"
 vim.opt.clipboard = "unnamedplus"
-
--- Lines
-vim.wo.number = true
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "number"
 vim.opt.wrap = false
-vim.opt.scrolloff = 5
-vim.opt.fillchars = { eob = " " }
 
 -- Tabs
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- Appearance
+vim.opt.title = true
+vim.wo.number = true
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.guicursor = ""
+vim.opt.colorcolumn = "120"
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+vim.opt.scrolloff = 5
+vim.opt.fillchars = { eob = " " }
 
 -- Diagnostics
 vim.diagnostic.config({
