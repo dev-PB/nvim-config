aab74f93e23b9d86ff4583416426f37f5711570a
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-04-19 22:11:29 +0100

    More updates to the Markview config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,3 +1,8 @@
+local heading_opts = {
+    sign = "",
+    style = "label",
+}
+
 return {
     "OXY2DEV/markview.nvim",
     lazy = false,
@@ -8,29 +13,28 @@ return {
 
     opts = {
         preview = {
+            icon_provider = "devicons",
         },
 
         markdown = {
             headings = {
                 shift_width = 0,
 
-                heading_1 = {
-                    sign = "",
-                    sign_hl = "",
-                    style = "label",
-                    align = "center",
-                },
-
-                heading_2 = {
-                    sign = "",
-                    sign_hl = "",
-                }
+                heading_1 = heading_opts,
+                heading_2 = heading_opts,
+                heading_3 = heading_opts,
+                heading_4 = heading_opts,
+                heading_5 = heading_opts,
             },
 
             list_items = {
                 wrap = true,
                 shift_width = 2,
             },
+
+            code_blocks = {
+                sign = false,
+            }
         },
     },
 };
