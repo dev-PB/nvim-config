69c51f568d267b8dc2606480e8c67a9ca7edf3bb
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-05-06 20:33:43 +0100

    Add Conform plugin for autoformatting

---
+++ lua/plugins/autoformatting.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "n",
+			desc = "Format buffer",
+		},
+	},
+
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+
+		format_on_save = { timeout_ms = 500 },
+
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt" },
+		},
+	},
+}
