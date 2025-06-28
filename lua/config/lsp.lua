3b686b4f1c7822f9010829fc293ce3c843b2c9f8
Ryan W <47390291+dev-PB@users.noreply.github.com>
2025-06-29 00:35:40 +0100

    Replace omnisharp with roslyn as our lsp for c#

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,7 @@
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("rust_analyzer")
-vim.lsp.enable("omnisharp")
 vim.lsp.enable("pyright")
 vim.lsp.enable("ts_ls")
+
+vim.env.PATH = vim.env.PATH .. ";C:/roslyn_lsp/"
+vim.lsp.enable("roslyn_ls")
